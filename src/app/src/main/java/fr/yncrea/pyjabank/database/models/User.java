013bc72951271cbe1892604dae6efe3027ff25ac
Thibaut <thibaut.juzeau@isen.yncrea.fr>
2021-01-31 13:32:22 +0100

    db migration ok, user with credentials

--- src/app/src/main/java/fr/yncrea/pyjabank/database/models/User.java
+++ src/app/src/main/java/fr/yncrea/pyjabank/database/models/User.java
@@ -10,6 +10,8 @@ public class User {
     private int id;
     private String name;
     private String lastname;
+    private String username;
+    private String password;
 
     public int getId() {
         return id;
@@ -33,5 +35,21 @@ public class User {
 
     public void setLastname(String lastname) {
         this.lastname = lastname;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
